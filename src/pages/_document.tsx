413df6b21b534f9734e8216e9d7a45e8bf604073
César Damasceno <64749161+ManeMaria@users.noreply.github.com>
2023-04-24 13:42:55 -0300

    fix: several fix by customer order

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,45 +1,13 @@
 /* eslint-disable @next/next/no-css-tags */
 import { Html, Head, Main, NextScript } from 'next/document';
 
+import { Tags } from '@/components/Tags';
+
 export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        <meta charSet="uf-8" />
-        <meta name="robots" content="index,follow" />
-
-        <meta property="og:url" content="https://www.sertanejasmaistocadas.com.br" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Top 100 músicas sertanejas" />
-        <meta
-          property="og:description"
-          content="Ouça agora as Top 100 músicas sertanejas mais tocadas de 2023."
-        />
-        <meta
-          property="og:image"
-          itemProp="image"
-          content="https://player-yt-spa.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffestival_widexl.4f8ed320.webp&w=800&q=75"
-        />
-
-        <meta name="twitter:card" content="summary_large_image" />
-
-        <meta name="twitter:title" content="Top 100 músicas sertanejas" />
-        <meta
-          name="twitter:description"
-          content="Ouça agora as Top 100 músicas sertanejas mais tocadas de 2023."
-        />
-        <meta
-          name="twitter:image"
-          content="https://player-yt-spa.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffestival_widexl.4f8ed320.webp&w=800&q=75"
-        />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ed0f35" />
-        <meta name="msapplication-TileColor" content="#2b5797" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="icon" href="/favicon.ico" />
+        <Tags />
       </Head>
       <body>
         <Main />
